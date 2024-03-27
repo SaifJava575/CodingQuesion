404e179985fb956a8487e5a0d0b0689fb539d0e7
SaifJava575 <sksaifuddin575@gmail.com>
2024-03-27 11:04:57 +0530

    Equilibrium point of the Arrays

--- src/com/nt/sdesheet/SD_04_EquilibriumPoint.java
+++ src/com/nt/sdesheet/SD_04_EquilibriumPoint.java
@@ -1,13 +1,26 @@
 package com.nt.sdesheet;
 
 public class SD_04_EquilibriumPoint {
-	
-	 public static int equilibriumPoint(long arr[], int n) {
-		 return 1;
-	    }
+
+	// T(c)=O(n) and S(C)=O(1)
+	public static int equilibriumPoint(long arr[], int n) {
+		int sum=0;
+		for(int i=0;i<n;i++) 
+			sum+=arr[i];
+		int sum1=0;
+		for(int j=0;j<n;j++) {
+			sum-=arr[j];
+			if(sum==sum1)
+				return j+1;
+			sum1+=arr[j];
+		}	
+		return -1;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		long A[] = { 1, 3, 5, 2, 2 };
+		int response = equilibriumPoint(A, A.length);
+		System.out.println("The Equilibrium point position is ::" + response);
 
 	}
 
